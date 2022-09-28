dc8df16de7c579e1072551042b9c95b931f84ff9
wildefran <104106774+wilduart@users.noreply.github.com>
2022-09-28 12:00:32 -0300

    usando o FOR
    Faça um programa que exiba os números de 1 até 2000.

---
+++ exercicios/Exer26.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package parimpar;
+
+/**
+ *
+ * @author admin
+ */
+public class Exer26 {
+    public static void main(String[] args) {
+        for(int i=1;i<=2000;i++){
+            System.out.println(i+" ");
+        }
+    }
+    
+}
